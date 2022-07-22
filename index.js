51d5f8f10f0e278eca373381bb0f20e4b1804aa5
gray <1012518214@qq.com>
2022-07-22 16:05:36 +0800

    learn

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
 function greed(person, date) {
-    return `asd ${person}, ${date.toDateString()}`;
+    return "asd " + person + ", " + date.toDateString();
 }
 greed("gray", new Date());
+// string number boolean
+var str = '123';
+var num = 223;
+var flag = false;
+var arr = [1, 23, 23];
+var a;
+function greet(name) {
+    return name;
+}
+greet('123');
+function printPoort(point) {
+    return point;
+}
+printPoort({ x: 1, y: 2 });
